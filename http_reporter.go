8691361c7e9d5f18622612bee9c5a67c7728d9a6
Bernardo <bernardo@Bernardos-MacBook-Pro.local>
2026-03-24 12:22:38 -0300

    debug(): Print body of report metrics request

--- http_reporter.go
+++ http_reporter.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"strings"
 	"time"
@@ -69,6 +70,8 @@ func (h *httpReporter) ReportMetrics(ctx context.Context, snapshot Snapshot) err
 	if err != nil {
 		return fmt.Errorf("error in marshal: %w", err)
 	}
+
+	slog.Info("body", "processed snapshot", string(body))
 
 	reportMetricsUrl := h.config.MetricsPath
 
