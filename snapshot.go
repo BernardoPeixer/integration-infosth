4228834febc3ebfa73923614549d8e4143766a8e
Bernardo <bernardo@Bernardos-MacBook-Pro.local>
2026-03-23 23:54:34 -0300

    fix(): Add json tag in structs

--- snapshot.go
+++ snapshot.go
@@ -3,21 +3,21 @@ package service_infosth
 import "time"
 
 type Snapshot struct {
-	Service     string
-	WindowStart time.Time
-	WindowEnd   time.Time
-	Items       []SnapshotAggregated
+	Service     string               `json:"service"`
+	WindowStart time.Time            `json:"window_start"`
+	WindowEnd   time.Time            `json:"window_end"`
+	Items       []SnapshotAggregated `json:"items"`
 }
 
 type SnapshotAggregated struct {
-	Route         string
-	Method        string
-	RequestCount  uint64
-	SuccessCount  uint64
-	ErrorCount    uint64
-	DurationSumMs uint64
-	MaxLatencyMs  uint64
-	Status2xx     uint64
-	Status4xx     uint64
-	Status5xx     uint64
+	Route         string `json:"route"`
+	Method        string `json:"method"`
+	RequestCount  uint64 `json:"request_count"`
+	SuccessCount  uint64 `json:"success_count"`
+	ErrorCount    uint64 `json:"error_count"`
+	DurationSumMs uint64 `json:"duration_sum_ms"`
+	MaxLatencyMs  uint64 `json:"max_latency_ms"`
+	Status2xx     uint64 `json:"status_2xx"`
+	Status4xx     uint64 `json:"status_4xx"`
+	Status5xx     uint64 `json:"status_5xx"`
 }
